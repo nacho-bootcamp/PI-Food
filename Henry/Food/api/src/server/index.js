b3bc3c8b36cc32aa77f62f63e7947b3175e1036c
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-04-12 15:43:29 -0300

    PI

---
+++ Henry/Food/api/src/server/index.js
@@ -0,0 +1,43 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require("./app");
+const sequelize = require("../database");
+
+function main() {
+  server.listen(3001, async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("estamos conectado");
+    } catch (error) {
+      console.log(error.message);
+    }
+    console.log("Listening in port 3001");
+  });
+}
+
+// const { conn } = require('./src/db.js');
+
+// // Syncing all the models at once.
+// conn.sync({ force: true }).then(() => {
+//   server.listen(3001, () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
+main();
