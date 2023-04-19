4bd41a998730678c293c8b16c6861476f1d18833
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-04-19 14:49:29 -0300

    database

--- src/controllers/recipeControllers.js
+++ src/controllers/recipeControllers.js
@@ -16,27 +16,28 @@ const createRecipe = async (
 
 module.exports = { createRecipe, getRecipebyId };
 
-// async function getRecipebyId(req, res) {
-//   const { id } = req.params;
-//   let recipe = {};
-//   try {
-//     const data = await recipe.findByPk(id);
-//     if (data) {
-//       let recipes = data;
-//       return res.status(200).send(recipes);
-//     }
-//     await axios(
-//       `https://api.spoonacular.com/recipes/${id}?key=77e0e45c970e4defa00a29287c4f3ede`.then(
-//         ({ data }) => {
-//           recipe = {
-//             id: data.id,
-//             name: data.name,
-//           };
-//         }
-//       )
-//     );
+async function getRecipebyId(req, res) {
+  const { id } = req.params;
+  let recipe = {};
+  try {
+    const data = await recipe.findByPk(id);
+    if (data) {
+      let recipes = data;
+      return res.status(200).send(recipes);
+    }
+    await axios(
+      `https://api.spoonacular.com/recipes/${id}?key=77e0e45c970e4defa00a29287c4f3ede`.then(
+        ({ data }) => {
+          recipe = {
+            id: data.id,
+            name: data.name,
+          };
+        }
+      )
+    );
 
-//     res.status(200).send("este esel id");
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
+    res.status(200).send("este esel id");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
